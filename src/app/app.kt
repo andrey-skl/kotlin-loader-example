7e72d0714f149adc3d8936f9a55061271a4b426a
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-08-13 23:06:45 +0300

    Add second file. Does not compiles by some reason

--- src/app/app.kt
+++ src/app/app.kt
@@ -1,10 +1,10 @@
-package org.sample
+package app
 
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
     document.getElementById("hello-world")?.innerHTML = "Welcome to Kotlin Webpack example"
-    println("Hello, world!")
+//    println(getGreeting())
 
     val el = document.createElement("div")
     el.appendChild(document.createTextNode("Hello!"))
---
+++ src/app/appHelper.kt
@@ -0,0 +1,9 @@
+package app
+
+fun getGreeting(): String {
+    val words = mutableListOf<String>()
+    words.add("Hello,")
+    words.add("world!")
+
+    return words.joinToString(separator = " ")
+}
